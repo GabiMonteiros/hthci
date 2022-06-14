08739f05fb4b37d67c340d95fe0b87ba238f9759
Gabi Monteiro <contatogabimonteiro@gmail.com>
2022-06-14 12:19:06 +0200

    css footer in progress

---
+++ activePage.js
@@ -0,0 +1,10 @@
+const { links } = require("express/lib/response");
+
+document.querySelectorAll('.nav-link').forEach(links => {
+    console.log(link);
+})
+
+
+
+//const activePage = window.location.pathname;
+//console.log(activePage);
